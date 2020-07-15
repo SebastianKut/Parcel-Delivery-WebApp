273251abb95b9218401359ce47e8b647ada283e4
sebastiankut <sebastian_kut@yahoo.co.uk>
2020-07-15 21:25:01 +0200

    orders list added

--- js/init.js
+++ js/init.js
@@ -51,6 +51,7 @@ addOrderLink.addEventListener('click', displayOrderForm);
 function displayOrderForm() {
 document.getElementById("welcome-message").style.display = "none";
 document.getElementById("order-sent").style.display = "none";
+document.getElementById("orders-list-user").style.display = "none";
 document.getElementById("send-parcel-form").style.display = "block";
 }
 
@@ -60,17 +61,32 @@ orderSubmitBtn.addEventListener('click', orderSent)
 function orderSent() {
 document.getElementById("order-sent").style.display = "block";
 document.getElementById("send-parcel-form").style.display = "none";
+document.getElementById("orders-list-user").style.display = "none";
+document.getElementById("welcome-message").style.display = "none";
 }
 
 let closeForm = document.getElementById("close-form");
-closeForm.addEventListener('click', closeInputForm) 
+closeForm.addEventListener('click', closeAllContent) 
 
-function closeInputForm(){
+function closeAllContent(){
+document.getElementById("order-sent").style.display = "none";
+document.getElementById("orders-list-user").style.display = "none";
 document.getElementById("send-parcel-form").style.display = "none";
 document.getElementById("welcome-message").style.display = "block";
 }
 
+let showAllOrdersLink = document.getElementById("show-all-orders-user");
+showAllOrdersLink.addEventListener('click', showAllOrders);
 
+function showAllOrders() {
+document.getElementById("order-sent").style.display = "none";
+document.getElementById("send-parcel-form").style.display = "none";
+document.getElementById("welcome-message").style.display = "none";
+document.getElementById("orders-list-user").style.display = "block";
+}
+
+let closeAllOrdersBtn = document.getElementById("close-table-user");
+closeAllOrdersBtn.addEventListener('click', closeAllContent); 
 });
 
 
