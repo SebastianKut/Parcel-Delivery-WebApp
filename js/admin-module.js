00a2c19be3783f9208d0b5303ccc1ce0a979bf70
sebastiankut <sebastian_kut@yahoo.co.uk>
2020-07-19 13:25:50 +0200

    sort function added to admin

--- js/admin-module.js
+++ js/admin-module.js
@@ -48,5 +48,79 @@ function displayOrderForm() {
     }
 
 //sort orders according to option selected
+let sortOrderList = document.getElementById('sort-orders');
+sortOrderList.addEventListener('change', filterItems);
+
+
+function filterItems(event){
+    let searchedOrder = event.target.value;
+    let allOrders = document.getElementsByClassName('order-wrapper');
+    console.log();
+
+    // helper function to compare senders 
+    function compareSenders(a, b) {
+        if (a.firstElementChild.firstElementChild.textContent > b.firstElementChild.firstElementChild.textContent) return 1;
+        if (b.firstElementChild.firstElementChild.textContent > a.firstElementChild.firstElementChild.textContent) return -1;
+        return 0;
+      } 
+// helper function to compare receivers
+    function compareReceivers(a, b) {
+        if (a > b) return 1;
+        if (b > a) return -1;
+        return 0;
+    } 
+
+        //show all
+        if (searchedOrder === 'all') {
+            Array.from(allOrders).forEach(order => {
+                console.log(searchedOrder);
+                order.style.display = 'table-row';
+            })
+        } 
+        //filter by inprogress and completed
+        if (searchedOrder === 'oczekujace' || searchedOrder === 'zrealizowane') {
+            Array.from(allOrders).forEach(order => {
+                if (order.children[5].textContent.toLowerCase().includes(searchedOrder.toLowerCase())) {
+                    order.style.display = 'table-row';
+                } else {
+                    order.style.display = 'none';
+                };
+            });
+        };     
+
+        //sort by sender name
+        if (searchedOrder === 'sender') {
+            //may have to first display none otherwise they will display as they are even when the array is sorted
+            //it doesnt work atm because the list is hardcoded in html, should work when created dynamically
+            // Array.from(allOrders).forEach(order =>{
+            //     order.style.display = 'none';    
+            // });
+           let sortedByName = Array.from(allOrders).sort(compareSenders);
+           console.log(sortedByName);
+           sortedByName.forEach(order => {
+            order.style.display = 'table-row';
+           })
+        }
+
+        //sort by receiver name
+        if (searchedOrder === 'receiver') {
+            //may have to first display none otherwise they will display as they are even when the array is sorted
+            //it doesnt work atm because the list is hardcoded in html, should work when created dynamically
+            // Array.from(allOrders).forEach(order =>{
+            //     order.style.display = 'none';    
+            // });
+           let sortedByName = Array.from(allOrders).sort(compareReceivers);
+           console.log(sortedByName);
+           sortedByName.forEach(order => {
+            order.style.display = 'table-row';
+           })
+        }
+
+
+}
+
+
+
+
 
 });
